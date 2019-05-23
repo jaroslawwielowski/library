1c7c2b8b0726c451b968a87073fcd13bc5286e28
jaroslawwielowski <jaroslawwielowski@gmail.com>
2019-05-23 10:15:20 +0200

    first today and repear dependency in book repository to book

--- src/main/java/com/library/app/entities/Book.java
+++ src/main/java/com/library/app/entities/Book.java
@@ -7,19 +7,57 @@ import javax.persistence.Id;
 
 @Entity
 public class Book {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long id;
-	
+
 	public String title;
 
 	public String author;
 
 	public String isbn;
-	
+
 	public String description;
 
-	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 }
--- src/main/java/com/library/app/repositories/BookRepository.java
+++ src/main/java/com/library/app/repositories/BookRepository.java
@@ -2,6 +2,8 @@ package com.library.app.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.library.app.entities.Book;
+
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
