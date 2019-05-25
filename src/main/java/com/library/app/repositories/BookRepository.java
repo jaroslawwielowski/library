693ca67c1bd0b9144ee1bee6be61a3e136a2164e
jaroslawwielowski <jaroslawwielowski@gmail.com>
2019-05-25 21:53:58 +0200

    new working dependencies and paskages

--- src/main/java/com/library/app/repositories/BookRepository.java
+++ src/main/java/com/library/app/repositories/BookRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.library.app.entities.Book;
 
-public interface BookRepository extends JpaRepository<Book, Long>{
+public interface BookRepository  extends JpaRepository<Book, Long>{
 
 }
